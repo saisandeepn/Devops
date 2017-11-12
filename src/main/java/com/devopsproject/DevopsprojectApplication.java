9afe8eca3cbb72bebe2a3e2e1adaa04a66179666
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-11-11 17:56:12 -0800

    ..

---
+++ src/main/java/com/devopsproject/DevopsprojectApplication.java
@@ -0,0 +1,12 @@
+package com.devopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsprojectApplication.class, args);
+	}
+}
